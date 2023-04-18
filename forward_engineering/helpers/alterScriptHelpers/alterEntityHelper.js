67c826a310ca2e7d37257120ed7281020bc256f4
unknown <unknown>
2023-04-18 16:05:10 +0300

    Copied fixes from PostgreSQL plugin

--- forward_engineering/helpers/alterScriptHelpers/alterEntityHelper.js
+++ forward_engineering/helpers/alterScriptHelpers/alterEntityHelper.js
@@ -112,35 +112,126 @@ const getDeleteColumnScript = app => collection => {
 		.map(([name]) => `ALTER TABLE IF EXISTS ${fullName} DROP COLUMN IF EXISTS ${wrapInQuotes(name)};`);
 };
 
-const getModifyColumnScript = app => collection => {
-	const _ = app.require('lodash');
-	const { getEntityName } = require('../../utils/general')(_);
-	const { getNamePrefixedWithSchemaName, wrapInQuotes } = require('../general')({ _ });
+const getFullTableName = (_) => (collection) => {
+	const {getEntityName} = require('../../utils/general')(_);
+	const {getNamePrefixedWithSchemaName} = require('../general')({_});
 
-	const collectionSchema = { ...collection, ...(_.omit(collection?.role, 'properties') || {}) };
+	const collectionSchema = {...collection, ...(_.omit(collection?.role, 'properties') || {})};
 	const tableName = getEntityName(collectionSchema);
 	const schemaName = collectionSchema.compMod?.keyspaceName;
-	const fullName = getNamePrefixedWithSchemaName(tableName, schemaName);
+	return getNamePrefixedWithSchemaName(tableName, schemaName);
+}
 
-	const renameColumnScripts = _.values(collection.properties)
-		.filter(jsonSchema => checkFieldPropertiesChanged(jsonSchema.compMod, ['name']))
-		.map(
-			jsonSchema =>
-				`ALTER TABLE IF EXISTS ${fullName} RENAME COLUMN ${wrapInQuotes(
-					jsonSchema.compMod.oldField.name,
-				)} TO ${wrapInQuotes(jsonSchema.compMod.newField.name)};`,
-		);
+const extractNewPropertyByName = (collection, fieldName) => {
+	return collection.role.compMod?.newProperties?.find(newProperty => newProperty.name === fieldName);
+}
+
+const hasLengthChanged = (collection, newFieldName, oldFieldName) => {
+	const oldProperty = collection.role.properties[oldFieldName];
+	const newProperty = extractNewPropertyByName(collection, newFieldName);
+
+	const previousLength = oldProperty?.length;
+	const newLength = newProperty?.length;
+	return previousLength !== newLength;
+}
+
+const hasPrecisionOrScaleChanged = (collection, newFieldName, oldFieldName) => {
+	const oldProperty = collection.role.properties[oldFieldName];
+	const newProperty = extractNewPropertyByName(collection, newFieldName);
+
+	const previousPrecision = oldProperty?.precision;
+	const newPrecision = newProperty?.precision;
+	const previousScale = oldProperty?.scale;
+	const newScale = newProperty?.scale;
+
+	return previousPrecision !== newPrecision || previousScale !== newScale;
+}
+
+const getUpdateTypesScripts = (_, ddlProvider) => (collection) => {
+	const fullTableName = getFullTableName(_)(collection);
+	const {wrapInQuotes} = require('../general')({_});
 
 	const changeTypeScripts = _.toPairs(collection.properties)
-		.filter(([name, jsonSchema]) => checkFieldPropertiesChanged(jsonSchema.compMod, ['type', 'mode']))
+		.filter(([name, jsonSchema]) => {
+			const hasTypeChanged = checkFieldPropertiesChanged(jsonSchema.compMod, ['type', 'mode']);
+			if (!hasTypeChanged) {
+				const oldName = jsonSchema.compMod.oldField.name;
+				const isNewLength = hasLengthChanged(collection, name, oldName);
+				const isNewPrecisionOrScale = hasPrecisionOrScaleChanged(collection, name, oldName);
+				return isNewLength || isNewPrecisionOrScale;
+			}
+			return hasTypeChanged;
+		})
 		.map(
-			([name, jsonSchema]) =>
-				`ALTER TABLE IF EXISTS ${fullName} ALTER COLUMN ${wrapInQuotes(name)} SET DATA TYPE ${
-					jsonSchema.compMod.newField.mode || jsonSchema.compMod.newField.type
-				};`,
+			([name, jsonSchema]) => {
+				const typeName = jsonSchema.compMod.newField.mode || jsonSchema.compMod.newField.type;
+				const columnName = wrapInQuotes(name);
+				const newProperty = extractNewPropertyByName(collection, name);
+				const typeConfig = _.pick(newProperty, ['length', 'precision', 'scale']);
+				return ddlProvider.alterColumnType(fullTableName, columnName, typeName, typeConfig);
+			}
 		);
+	return [...changeTypeScripts];
+}
 
-	return [...renameColumnScripts, ...changeTypeScripts];
+const getModifyNonNullColumnsScripts = (_, ddlProvider) => (collection) => {
+	const fullTableName = getFullTableName(_)(collection);
+	const {wrapInQuotes} = require('../general')({_});
+
+	const currentRequiredColumnNames = collection.required || [];
+	const previousRequiredColumnNames = collection.role.required || [];
+
+	const columnNamesToAddNotNullConstraint = _.difference(currentRequiredColumnNames, previousRequiredColumnNames);
+	const columnNamesToRemoveNotNullConstraint = _.difference(previousRequiredColumnNames, currentRequiredColumnNames);
+
+	const addNotNullConstraintsScript = _.toPairs(collection.properties)
+		.filter(([name, jsonSchema]) => {
+			const oldName = jsonSchema.compMod.oldField.name;
+			const shouldRemoveForOldName = columnNamesToRemoveNotNullConstraint.includes(oldName);
+			const shouldAddForNewName = columnNamesToAddNotNullConstraint.includes(name);
+			return shouldAddForNewName && !shouldRemoveForOldName;
+		})
+		.map(([columnName]) => ddlProvider.setNotNullConstraint(fullTableName, wrapInQuotes(columnName)));
+	const removeNotNullConstraint = _.toPairs(collection.properties)
+		.filter(([name, jsonSchema]) => {
+			const oldName = jsonSchema.compMod.oldField.name;
+			const shouldRemoveForOldName = columnNamesToRemoveNotNullConstraint.includes(oldName);
+			const shouldAddForNewName = columnNamesToAddNotNullConstraint.includes(name);
+			return shouldRemoveForOldName && !shouldAddForNewName;
+		})
+		.map(([name]) => ddlProvider.dropNotNullConstraint(fullTableName, wrapInQuotes(name)));
+
+	return [...addNotNullConstraintsScript, ...removeNotNullConstraint];
+}
+
+const getRenameColumnScripts = (_, ddlProvider) => (collection) => {
+	const fullTableName = getFullTableName(_)(collection);
+	const {wrapInQuotes} = require('../general')({_});
+
+	return _.values(collection.properties)
+		.filter(jsonSchema => checkFieldPropertiesChanged(jsonSchema.compMod, ['name']))
+		.map(
+			jsonSchema => {
+				const oldColumnName = wrapInQuotes(jsonSchema.compMod.oldField.name);
+				const newColumnName = wrapInQuotes(jsonSchema.compMod.newField.name);
+				return ddlProvider.renameColumn(fullTableName, oldColumnName, newColumnName);
+			}
+		);
+}
+
+const getModifyColumnScript = app => collection => {
+	const _ = app.require('lodash');
+	const ddlProvider = require('../../ddlProvider')(null, null, app);
+
+	const renameColumnScripts = getRenameColumnScripts(_, ddlProvider)(collection);
+	const updateTypeScripts = getUpdateTypesScripts(_, ddlProvider)(collection);
+	const modifyNotNullScripts = getModifyNonNullColumnsScripts(_, ddlProvider)(collection);
+
+	return [
+		...renameColumnScripts,
+		...updateTypeScripts,
+		...modifyNotNullScripts
+	];
 };
 
 module.exports = {
